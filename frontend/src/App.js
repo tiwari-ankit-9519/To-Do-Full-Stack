bee66e67cc7f3566e0d9de56ac98089bc1d02e0d
Ankit Tiwari <tiwari.ankit3105@gmail.com>
2024-06-03 23:35:36 +0530

    Third Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateTodo from "./components/CreateTodo";
 import Todos from "./components/Todos";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-  fetch("http://localhost:4000/todos").then(async (res) => {
-    const json = await res.json();
-    setTodos(json.todos);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:4000/todos").then(async (res) => {
+        const json = await res.json();
+        setTodos(json);
+      });
+    }, 2000);
   });
 
   return (
